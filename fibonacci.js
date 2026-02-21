765d43f81beebc98ed9ff354f10692fa85b5ea9f
max-ldx <242964282+max-ldx@users.noreply.github.com>
2026-02-21 16:53:42 +0100

    feat: add fibsRec

--- fibonacci.js
+++ fibonacci.js
@@ -1,16 +1,30 @@
-export function fibs(number) {
-    if (number <= 0) return undefined;
-    if (number === 1) return [0];
+export function fibs(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
 
     let first = 0;
     let second = 1;
 
     const result = [first, second];
 
-    for (let i = 2; i < number; i++) {
+    for (let i = 2; i < n; i++) {
         result.push(first + second);
         [first, second] = [second, second + first];
     }
 
     return result;
+}
+
+export function fibsRec(n, array = [0, 1]) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+
+    if (array.length >= n) {
+        return array;
+    }
+
+    const nextVal = array[array.length - 1] + array[array.length - 2];
+    array.push(nextVal);
+
+    return fibsRec(n, array);
 }
--- fibonacci.test.js
+++ fibonacci.test.js
@@ -1,13 +1,13 @@
 import { describe, expect, test } from "vitest";
-import { fibs } from "./fibonacci";
+import { fibs, fibsRec } from "./fibonacci";
 
 describe("fibs", () => {
-    test("0 returns undefined", () => {
-        expect(fibs(0)).toStrictEqual(undefined);
+    test("0 returns []", () => {
+        expect(fibs(0)).toStrictEqual([]);
     });
 
-    test("negative number returns undefined", () => {
-        expect(fibs(-1)).toStrictEqual(undefined);
+    test("negative number returns []", () => {
+        expect(fibs(-1)).toStrictEqual([]);
     });
 
     test("1 returns [0]", () => {
@@ -24,5 +24,31 @@ describe("fibs", () => {
 
     test("10, returns [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]", () => {
         expect(fibs(10)).toStrictEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
+    });
+});
+
+describe("fibsRec", () => {
+    test("0 returns []", () => {
+        expect(fibsRec(0)).toStrictEqual([]);
+    });
+
+    test("negative number []", () => {
+        expect(fibsRec(-1)).toStrictEqual([]);
+    });
+
+    test("1 returns [0]", () => {
+        expect(fibsRec(1)).toStrictEqual([0]);
+    });
+
+    test("2, returns [0, 1]", () => {
+        expect(fibsRec(2)).toStrictEqual([0, 1]);
+    });
+
+    test("8, returns [0, 1, 1, 2, 3, 5, 8, 13]", () => {
+        expect(fibsRec(8)).toStrictEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+    });
+
+    test("10, returns [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]", () => {
+        expect(fibsRec(10)).toStrictEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
     });
 });
